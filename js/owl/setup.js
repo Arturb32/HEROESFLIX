f384ac1cc5d5fce64fd1b65c6d76c72c5f737433
Arturb32 <arturbernardo49@gmail.com>
2021-07-18 13:53:14 -0300

    Commit inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+function info(){
+    window.open("https://www.adorocinema.com/series/serie-24434/");
+}
+function trailer(){
+    window.open("https://www.youtube.com/watch?v=i2qDt5D8RSs");
+}
